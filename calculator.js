ece537be4fc5239b89936d5a9cf925f42eee0392
vani11 <anshuvanitha@gmail.com>
2017-02-07 16:15:17 -0500

    Calculator using angular js

---
+++ calculator.js
@@ -0,0 +1,34 @@
+﻿var myApp = angular.module('myApp', []);
+
+myApp.controller('calculatorCtrl', function ($scope) {
+
+    function add(x, y) {
+        return x + y;
+    }
+
+    function sub(x, y) {
+        return x - y;
+    }
+
+    function mul(x, y) {
+        return x * y;
+    }
+
+    function div(x, y) {
+        return x / y;
+    }
+
+    function calc(op, x, y) {
+        return $scope.operators[op](parseInt(x, 10), parseInt(y));
+    }
+
+    $scope.operators = {
+        '+': add,
+        '-': sub,
+        '*': mul,
+        '/': div
+    };
+    $scope.op = '+';
+    $scope.calc = calc;
+
+});
